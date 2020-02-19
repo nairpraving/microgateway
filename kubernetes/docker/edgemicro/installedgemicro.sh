b7ecf8f95715058900e9bfe564de5c46f8bfd1e2
nairpraving <pravin.mail@gmail.com>
2020-02-19 17:49:01 -0500

    Update installedgemicro.sh

--- kubernetes/docker/edgemicro/installedgemicro.sh
+++ kubernetes/docker/edgemicro/installedgemicro.sh
@@ -5,4 +5,4 @@ set echo off
     mkdir -p /opt/apigee/logs && \
     mkdir -p /opt/apigee/plugins && \
     edgemicro init && \
-    chmod +x /opt/apigee/entrypoint.sh
+    #chmod +x /opt/apigee/entrypoint.sh
